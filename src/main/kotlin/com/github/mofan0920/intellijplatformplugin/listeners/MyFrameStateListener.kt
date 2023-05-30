2d48b1db36b2badc1f6660c47311f0b07f4eae56
GitHub Action <action@github.com>
2023-05-30 03:11:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/mofan0920/intellijplatformplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/mofan0920/intellijplatformplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mofan0920.intellijplatformplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
